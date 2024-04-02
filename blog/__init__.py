ce2e803ec8aa8bd59fe52f6291199b064410051b
cAtaman <chimaataman@gmail.com>
2024-04-02 18:20:08 +0100

    run ruff

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,17 +1,17 @@
 import sys
 
-from blog.builder import run_make_mode, autobuild_main
+from blog.builder import autobuild_main, run_make_mode
 
 
-def main():
-    """Custom main method to format Make mode help text with the "-M" option"""
+def main() -> int | None:
+    """Patched entrypoint to format Make mode help text with the "-M" option."""
     args = sys.argv
-    if args[1:3] == ['-M', "help"]:
+    if args[1:3] == ["-M", "help"]:
         builder_args = args[2:]
         return run_make_mode(builder_args)
     else:
         return autobuild_main()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
